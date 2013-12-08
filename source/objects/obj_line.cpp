e6aa6687e015143f34b6c1013b4fd58a9d067427
Alex Han <link-5@mail.ru>
2013-12-08 07:00:58 +0400

    modified obj_line for new entity realization

--- source/objects/obj_line.cpp
+++ source/objects/obj_line.cpp
@@ -6,15 +6,25 @@ namespace
 };
 
 obj_line::obj_line(b2Vec2 _pos, b2Vec2 _size, cocos2d::ccColor4F _color, b2Vec2 _vel, b2Vec2 _acc)
-: entity(_pos, _size, OBJECT_FOREVER_LIFE, _vel, _acc)
-{    
+: entity(_pos, b2Vec2(ax::Abs(_size.x), ax::Abs(_size.y)), OBJECT_FOREVER_LIFE, _vel, _acc)
+{
+  direct = _size - _pos;
+
   setColor(_color);
 }
 
-void obj_line::OnUpdate(double dt)
+b2Vec2 obj_line::Size()
+{
+  //b2Vec2 _size = entity::Size();
+  //size = abs(_size)
+  //direct = (_size)
+  return entity::Size();
+}
+
+void obj_line::OnDraw()
 {
   cocos2d::ccDrawColor4F(color.r, color.g, color.b, color.a);
-  auto end = Position() + Size();
+  b2Vec2 end = Position() + direct;
   cocos2d::ccDrawLine(convert<cocos2d::CCPoint>(Position()), convert<cocos2d::CCPoint>(end));
 }
 
@@ -27,3 +37,10 @@ void obj_line::setColor(cocos2d::ccColor4F _color)
 {
   color = _color;
 }
+
+/*
+//CCLabelBMFont *label = CCLabelBMFont::create("bitmap", "test.fnt");
+CCLabelAtlas *label = CCLabelAtlas::create("a123", "test.png", 20, 30, 'a');
+CCDirector::sharedDirector()->getRunningScene()->addChild(label);
+//CCSprite *char_B = label->getChildByTag(0);
+*/
--- source/objects/obj_line.h
+++ source/objects/obj_line.h
@@ -5,9 +5,11 @@ class obj_line : public entity
 {
 private:
   cocos2d::ccColor4F color;
+  b2Vec2 direct;
 public:
   obj_line::obj_line(b2Vec2 _pos, b2Vec2 _size, cocos2d::ccColor4F _color, b2Vec2 _vel = b2Vec2_zero, b2Vec2 _acc = b2Vec2_zero);
-  void OnUpdate(double dt);
+  void OnDraw();
+  b2Vec2 obj_line::Size();
 
   cocos2d::ccColor4F obj_line::getColor();
   void setColor(cocos2d::ccColor4F _color);
