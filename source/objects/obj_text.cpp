01e8baec247e90c9e0c45727981ad66a24aae552
Alex Han <link-5@mail.ru>
2013-12-08 10:56:27 +0400

    obj_text modify for external string

--- source/objects/obj_text.cpp
+++ source/objects/obj_text.cpp
@@ -5,12 +5,12 @@ namespace
   const float32 OBJECT_FOREVER_LIFE = 23;
 };
 
-cocos2d::CCSprite* obj_text::spriteFromStr(std::string _str)
+cocos2d::CCNode* obj_text::spriteFromStr(std::string _str)
 {
-  cocos2d::CCLabelBMFont *label = cocos2d::CCLabelBMFont::create("FUCK!", "java.fnt");// 85, kCCTextAlignmentLeft);
+  cocos2d::CCLabelBMFont *label = cocos2d::CCLabelBMFont::create( _str.c_str(), "java.fnt");// 85, kCCTextAlignmentLeft);
   //label->setPositionX(100);
-  //label->setPositionY(100);
-  return (cocos2d::CCSprite*)!label;
+  //label->setPositionY(100);                                         s
+  return label;
 }
 
 obj_text::obj_text(std::string _str, b2Vec2 _pos, cocos2d::ccColor4F _color, b2Vec2 _vel, b2Vec2 _acc)
