f494079f72d75d8d6709df00a6eaa850469dc2a5
Alex Han <link-5@mail.ru>
2013-12-09 10:31:28 +0400

    refuse from esperanto at all
    ...for some time

--- source/objects/buildings/kerno.h
+++
@@ -1,9 +0,0 @@
-#include "conectNode.h"
-
-class kerno : public conectNode
-{
-public:
-
-  kerno::kerno(b2Vec2 pos);
-
-};
--- source/objects/buildings/transmitter.cpp
+++ source/objects/buildings/transmitter.cpp
@@ -2,17 +2,17 @@
 
 namespace
 {
-  const float32 KERNO_DEFAULT_LIFE = 17;
-  const b2Vec2 KERNO_DEFAULT_SIZE = b2Vec2(32, 32);
-  const word KERNO_DEFAULT_ENERGY_MAX = 0;
-  const int KERNO_DEFAULT_ENERGY_DELTA = -1;
-  const word KERNO_DEFAULT_DISTANCE = 65;
-  const _STL::string KERNO_DEFAULT_SPRITE_ADRESS = "transmitter.png";
+  const float32 transmitter_DEFAULT_LIFE = 17;
+  const b2Vec2 transmitter_DEFAULT_SIZE = b2Vec2(32, 32);
+  const word transmitter_DEFAULT_ENERGY_MAX = 0;
+  const int transmitter_DEFAULT_ENERGY_DELTA = -1;
+  const word transmitter_DEFAULT_DISTANCE = 65;
+  const _STL::string transmitter_DEFAULT_SPRITE_ADRESS = "transmitter.png";
 };
 
 transmitter::transmitter(b2Vec2 _pos)
-: conectNode(KERNO_DEFAULT_SPRITE_ADRESS, KERNO_DEFAULT_DISTANCE, _pos, KERNO_DEFAULT_SIZE, KERNO_DEFAULT_LIFE,
-KERNO_DEFAULT_ENERGY_MAX, KERNO_DEFAULT_ENERGY_DELTA)
+: conectNode(transmitter_DEFAULT_SPRITE_ADRESS, transmitter_DEFAULT_DISTANCE, _pos, transmitter_DEFAULT_SIZE, transmitter_DEFAULT_LIFE,
+transmitter_DEFAULT_ENERGY_MAX, transmitter_DEFAULT_ENERGY_DELTA)
 {
 
 }
